c9c81178b8006959d365ec78abbfa6ba71881d3c
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2022-11-19 05:41:08 +0300

    Unneccesarry usings removed

--- AuthServer/Config/Config.cs
+++ AuthServer/Config/Config.cs
@@ -1,11 +1,8 @@
-﻿using IdentityServer4.Models;
+﻿using IdentityServer4;
+using IdentityServer4.Models;
 using IdentityServer4.Test;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Security.Claims;
-using IdentityServer4;
 
 namespace AuthServer
 {
