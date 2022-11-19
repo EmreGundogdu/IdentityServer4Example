43610c2341810acb23bc87549340970c0489c558
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2022-11-19 13:08:04 +0300

    Online Bankamatik controller updated

--- OnlineBankamatik/Controllers/BankamatikController.cs
+++ OnlineBankamatik/Controllers/BankamatikController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Authentication;
+﻿using AuthServer;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
@@ -9,7 +10,6 @@ namespace OnlineBankamatik.Controllers
 {
     public class BankamatikController : Controller
     {
-
         public IActionResult Index()
         {
             return View();
