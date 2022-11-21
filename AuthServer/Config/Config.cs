8548190f55e178d674b7e627f224dc2a444db206
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2022-11-21 18:05:02 +0300

    Config updated

--- AuthServer/Config/Config.cs
+++ AuthServer/Config/Config.cs
@@ -13,12 +13,12 @@ namespace AuthServer
         public static IEnumerable<ApiScope> GetApiScopes()
         {
             return new List<ApiScope>
-            {
-                new ApiScope("Garanti.Write","Garanti bankası yazma izni"),
-                new ApiScope("Garanti.Read","Garanti bankası okuma izni"),
-                new ApiScope("HalkBank.Write","HalkBank bankası yazma izni"),
-                new ApiScope("HalkBank.Read","HalkBank bankası okuma izni"),
-            };
+        {
+            new ApiScope("Garanti.Write","Garanti bankası yazma izni"),
+            new ApiScope("Garanti.Read","Garanti bankası okuma izni"),
+            new ApiScope("HalkBank.Write","HalkBank bankası yazma izni"),
+            new ApiScope("HalkBank.Read","HalkBank bankası okuma izni"),
+        };
         }
         #endregion
         #region Resources
@@ -26,26 +26,10 @@ namespace AuthServer
         public static IEnumerable<ApiResource> GetApiResources()
         {
             return new List<ApiResource>
-            {
-                new ApiResource("Garanti"){
-                    ApiSecrets = {
-                        new Secret("garanti".Sha256())
-                    },
-                    Scopes = {
-                        "Garanti.Write",
-                        "Garanti.Read"
-                    }
-                },
-                new ApiResource("HalkBank"){
-                    ApiSecrets = {
-                        new Secret("halkbank".Sha256())
-                    },
-                    Scopes = {
-                        "HalkBank.Write",
-                        "HalkBank.Read"
-                    }
-                }
-            };
+        {
+            new ApiResource("Garanti"){ Scopes = { "Garanti.Write", "Garanti.Read" } },
+            new ApiResource("HalkBank"){ Scopes = { "HalkBank.Write", "HalkBank.Read" } }
+        };
         }
         #endregion
         #region Clients
@@ -53,39 +37,24 @@ namespace AuthServer
         public static IEnumerable<Client> GetClients()
         {
             return new List<Client>
-            {
-                new Client
-                        {//Muhasebe projesi için kullanıcak client 
-                            ClientId = "GarantiBankasi",
-                            ClientName = "GarantiBankasi",
-                            RequireClientSecret = false,
-                            AllowedGrantTypes = GrantTypes.Code,
-                            AllowedScopes = { "Garanti.Write", "Garanti.Read" }
-                        },
-                new Client
-                        { //Gencay Yıldız Örnek
-                            ClientId = "HalkBankasi",
-                            ClientName = "HalkBankasi",
-                            ClientSecrets = { new Secret("halkbank".Sha256()) },
-                            AllowedGrantTypes =  {GrantType.ClientCredentials},
-                            AllowedScopes = { "HalkBank.Write", "HalkBank.Read" }
-                            //AccessTokenLifetime =  2*60*60
-                        },
-                    new Client
+        {
+            new Client
                     {
-                        ClientId = "OnlineBankamatik",
-                        ClientName = "OnlineBankamatik",
-                        ClientSecrets = { new Secret("onlinebankamatik".Sha256()) },
-                        AllowedGrantTypes = IdentityServer4.Models.GrantTypes.Hybrid, //"code id_token'a karşılık gelen"
-                        AllowedScopes = { IdentityServerConstants.StandardScopes.OpenId, IdentityServerConstants.StandardScopes.Profile, IdentityServerConstants.StandardScopes.OfflineAccess,"Garanti.Write","Garanti.Read" },RedirectUris = { "https://localhost:4000/signin-oidc" },
-                        RequirePkce = false,
-                        AllowOfflineAccess = true,
-                        RefreshTokenUsage = TokenUsage.OneTimeOnly,
-                        RefreshTokenExpiration = TokenExpiration.Absolute,
-                        AbsoluteRefreshTokenLifetime = 2 * 60 * 60 + (10 * 60),
-                        RequireConsent = true
+                        ClientId = "GarantiBankasi",
+                        ClientName = "GarantiBankasi",
+                        ClientSecrets = { new Secret("garanti".Sha256()) },
+                        AllowedGrantTypes = { GrantType.ClientCredentials },
+                        AllowedScopes = { "Garanti.Write", "Garanti.Read" }
+                    },
+            new Client
+                    {
+                        ClientId = "HalkBankasi",
+                        ClientName = "HalkBankasi",
+                        ClientSecrets = { new Secret("halkbank".Sha256()) },
+                        AllowedGrantTypes = { GrantType.ClientCredentials },
+                        AllowedScopes = { "HalkBank.Write", "HalkBank.Read" }
                     }
-            };
+        };
         }
         #endregion
         public static IEnumerable<TestUser> GetTestUser()
